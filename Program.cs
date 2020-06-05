50ec4cccec2ba707d356d8854af806e689272c70
ViniciusMirandaDev <ViniciusMirandaDev>
2020-06-05 15:51:41 -0300

    Projeto Iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_TesteUm_Calculadoras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
